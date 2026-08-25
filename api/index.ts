2cbf357dad0e62e73f5578ed386e8513120fa9eb
diyaa_altmeme <acc.alrwdaten@gmail.com>
2026-08-25 16:31:36 +0300

    Enable global CORS headers and OPTIONS preflight support for Vercel API

--- api/index.ts
+++ api/index.ts
@@ -35,6 +35,19 @@ async function bootstrap() {
 }
 
 export default async function handler(req: Request, res: Response) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, x-branch-id'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (!isReady) {
     await bootstrap();
   }
